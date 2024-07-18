fc3e97d712658c3ec14a6e20dcbe51b0662e24e3
Kuzey Sınay <kuzeysinay@gmail.com>
2024-07-18 18:37:33 +0300

    add stacked center carousel for landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import LandingPage from './Pages/landingPage';
 
 function App() {
   return (
     <div className="font-sans">
+      <LandingPage></LandingPage>
     </div>
   );
 }
---
+++ src/Pages/landingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+
+const LandingPage = () => {
+  return (
+
+    <div>
+        <Carousel ></Carousel>
+    </div>
+  )
+}
+
+export default LandingPage
